57274cd885f90cec4c57369a4d928d3a4fa5a4ef
projjwalroychowdhury01 <projjwalroychowdhury@gmail.com>
2026-02-13 03:38:21 -0500

    Revert "added additional lin to Shipping_Cost_Calculator.py"
    This reverts commit b6545ac6d35a1169a47d1a01eb37f95d1a15ddfe.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -2,8 +2,6 @@
 
  # Here is a new update by <projjwalroychowdhury01>
 
-# Here is another update by <your GitHub username>
-
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
 rate = float(input("Enter the shipping rate per kilogram: "))
